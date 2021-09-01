9f1b80f11143454f6fd23dd18f87511c15f632ec
Rocknwolf <werwolf1488@hotmail.com>
2021-09-01 15:48:11 +0200

    added_static_json

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,41 @@
 const express = require("express");
+const cors = require("cors");
 
 const server = express();
 const app = server;
 
 const PORT = 3030;
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
   
 app.listen(PORT, () => {
 console.log(`Example app listening at http://localhost:${PORT}`)
 })
 
 server.use(express.json());
+server.use(cors());
 server.use(express.urlencoded({ extended: true }));
+
+const articles = [
+    {
+        link: "article1",
+        screenshot: "https://freibier-party.de/",
+        title: "Bier",
+        description: "lorem ipsum"
+    },
+    {
+        link: "article2",
+        screenshot: "http://www.biersekte.de/",
+        title: "mehr Bier",
+        description: "lorem ipsum"
+    },
+    {
+        link: "article3",
+        screenshot: "https://www.youtube.com/watch?v=l7DCHLaNWcA",
+        title: "am biersten",
+        description: "lorem ipsum"
+    },
+];
+
+server.get("/articles", (req, res) => {
+    res.send(articles);
+});
+
