bb1cef5092b69ca679a2d7a779324e5203f07be3
LexLuthor-official <72547569+LexLuthor-official@users.noreply.github.com>
2021-09-01 14:40:11 +0200

    Local Storage

--- tinyurl/src/App.js
+++ tinyurl/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Home from './Components/Home.jsx';
 import Error from './Components/Error.jsx';
 import Login from './Components/Login.jsx';
-
+import Form from './Form.jsx';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
@@ -13,6 +13,7 @@ function App() {
     <div className="app">
       <Router>
       <Switch>
+      <Route path="/Form" component={Form} />  
       <Route exact path="/" component={Home} />
       <Route path="/Login" component={Login} />
       <Route component={Error} />
